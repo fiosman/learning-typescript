9325236b821062276638b784f6c95bb1caa7dc9a
fiosman <fares.osman@outlook.com>
2023-12-16 17:39:45 -0500

    adds progress

---
+++ 02-prototypes-to-classes.js
@@ -0,0 +1,20 @@
+class Robot {
+  constructor(name, abilities) {
+    this.name = name;
+    this.abilities = abilities;
+    this.power = 100;
+  }
+
+  announce = () => {
+    console.log("Greetings. I am " + this.name + ".");
+
+    for (const ability of this.abilities) {
+      console.log("I am able to " + this.abilities[i] + ".");
+    }
+  };
+
+  charge = (amount) => {};
+}
+
+module.exports.Humanoid = Humanoid;
+module.exports.Robot = Robot;
