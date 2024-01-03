dae5b687d71b8e793abcfa6fbb1e39cae651022f
fiosman <fares.osman@outlook.com>
2024-01-02 23:12:00 -0500

    adds logic to roll a number

--- 03_unions_and_literals/04-narrow-trail.ts
+++ 03_unions_and_literals/04-narrow-trail.ts
@@ -2,4 +2,8 @@ export function runCommands() {
   let availableReSource: "food" | "water" | undefined;
   let food: number = 5;
   let water: number = 5;
+
+  for (let i = 1; i < 8; i++) {
+    const roll = Math.floor(Math.random() * 7);
+  }
 }
