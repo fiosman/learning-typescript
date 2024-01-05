46990ec016dc4098722409efda3bc4d183a70828
fiosman <fares.osman@outlook.com>
2024-01-05 00:10:06 -0500

    finish game logic

--- 03_unions_and_literals/04-narrow-trail.ts
+++ 03_unions_and_literals/04-narrow-trail.ts
@@ -5,17 +5,20 @@ export function runCommands() {
 
   for (let i = 1; i < 8; i++) {
     const roll = Math.floor(Math.random() * 7);
-
     if (roll >= 3 && roll <= 6) {
-      if (roll % 2 === 0) {
-        availableReSource = "food";
+      if (!availableReSource) {
+        roll % 2 === 0 ? (availableReSource = "food") : (availableReSource = "water");
       } else {
-        availableReSource = "water";
+        availableReSource === "food" ? (food += roll) : (water += roll);
+        availableReSource = undefined;
       }
     }
-    food -= 1;
-    water -= 1;
+    if (food === 0 || water === 0) {
+      return false;
+    } else {
+      food -= 1;
+      water -= 1;
+    }
   }
-
   return true;
 }
