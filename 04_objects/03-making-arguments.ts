96b0003e5ab635bebda99a5fe201de958e6ed418
fiosman <fares.osman@outlook.com>
2024-01-13 22:21:08 -0500

    adds progress to exercise

---
+++ 04_objects/03-making-arguments.ts
@@ -0,0 +1,105 @@
+export type PreTrialMotion = {
+  step: "pre-trial";
+  classification: "dissmis" | "suppress" | "venue";
+};
+
+export type PostTrialMotion = {
+  step: "post-trial";
+  classification: "acquittal" | "correction" | "new trial";
+};
+
+export type TrialMotion = (PreTrialMotion | PostTrialMotion) & {
+  from: "defendant" | "plaintiff";
+  reason: string;
+};
+
+export type AllowedMotion = TrialMotion & {
+  status: "allowed";
+  deliberationHours: number;
+};
+
+export type DeniedMotion = TrialMotion & {
+  status: "denied";
+  deliberationHours: number;
+  annoyedJustice: boolean;
+};
+
+export type PendingMotion = TrialMotion & {
+  status: "pending";
+  deliberationHours: number;
+};
+
+export type Motion = AllowedMotion | DeniedMotion | PendingMotion;
+
+export const motions: Motion[] = [
+  {
+    annoyedJustice: true,
+    classification: "acquittal",
+    deliberationHours: 1,
+    from: "defendant",
+    reason: "The heretofore document had dried ink on it.",
+    status: "denied",
+    step: "post-trial",
+  },
+  {
+    annoyedJustice: true,
+    classification: "correction",
+    deliberationHours: 2.5,
+    from: "plaintiff",
+    reason: "The tenant has ninety days to vacate.",
+    status: "denied",
+    step: "post-trial",
+  },
+  {
+    classification: "suppress",
+    deliberationHours: 4,
+    from: "plaintiff",
+    reason: "Frank was never allowed in the house.",
+    status: "allowed",
+    step: "pre-trial",
+  },
+  {
+    classification: "new trial",
+    estimatedDeliberationHours: 3,
+    from: "defendant",
+    reason: "The duel's been accepted. There's no backing out. That's the law.",
+    status: "pending",
+    step: "post-trial",
+  },
+  {
+    annoyedJustice: false,
+    classification: "dismiss",
+    deliberationHours: 0.5,
+    from: "plaintiff",
+    reason: "It seems like you have a tenuous grasp on the English language in general.",
+    status: "denied",
+    step: "pre-trial",
+  },
+  {
+    annoyedJustice: true,
+    classification: "correction",
+    deliberationHours: 1.5,
+    from: "defendant",
+    reason: "Fillibuster?",
+    status: "denied",
+    step: "post-trial",
+  },
+  {
+    annoyedJustice: true,
+    classification: "venue",
+    deliberationHours: 0.25,
+    from: "defendant",
+    reason: "A time was never specified for the duel.",
+    status: "denied",
+    step: "pre-trial",
+  },
+  {
+    annoyedJustice: true,
+    classification: "correction",
+    deliberationHours: 5,
+    from: "plaintiff",
+    reason: "He's making a few good points!",
+    status: "denied",
+    step: "post-trial",
+  },
+];
