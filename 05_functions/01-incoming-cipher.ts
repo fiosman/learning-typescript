3bc7282a8b3abe2085a50f3e7c31cfeb8b2dd943
fiosman <fares.osman@outlook.com>
2024-01-23 23:08:36 -0500

    finish exercise

--- 05_functions/01-incoming-cipher.ts
+++ 05_functions/01-incoming-cipher.ts
@@ -1,3 +1,11 @@
 export type Cipher = (str: string) => string;
 
-const createCipher = (cipher: Cipher) => {};
+const createCipher = (cipher: Cipher) => {
+  return (text: string): string => {
+    let result: string = "";
+    for (const char of text) {
+      result += cipher(char);
+    }
+    return result;
+  };
+};
