08dfb1e9b52cd2fd9b1bf57f309b65c7ab98da4a
fiosman <fares.osman@outlook.com>
2024-01-27 23:02:53 -0500

    finish function

--- 05_functions/02-dr-on.ts
+++ 05_functions/02-dr-on.ts
@@ -1,5 +1,20 @@
 export type Cipher = (str: string) => string;
 
 const createAdvancedCipher = (onVowel: Cipher, onConsonant: Cipher, onPunctuation: Cipher) => {
-  return (text: string): string => {};
+  return (text: string): string => {
+    const vowel = /[aeiou]/i;
+    const consonant = /[bcdfghjklmnpqrstvwxyz]/i;
+    let finalStr: string = "";
+    for (const char of text) {
+      if (vowel.test(char)) {
+        finalStr += onVowel(char);
+      } else if (consonant.test(char)) {
+        finalStr += onConsonant(char);
+      } else {
+        finalStr += onPunctuation(char);
+      }
+    }
+
+    return finalStr;
+  };
 };
