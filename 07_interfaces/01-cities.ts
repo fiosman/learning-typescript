7e11b63300b293045e1ff7a97915f3d3264e23ef
fiosman <fares.osman@outlook.com>
2024-02-08 23:03:11 -0500

    adds the interface

--- 07_interfaces/01-cities.ts
+++ 07_interfaces/01-cities.ts
@@ -1,3 +1,10 @@
-interface City {}
+interface City {
+  name: string;
+  coordinates: {
+    north: [number, number, number];
+    west: [number, number, number];
+  };
+  catchphrase?: string;
+}
 
 const describeCity = () => {};
