7845dcb84df28b3de4361867b3d8330f5da5a433
fiosman <fares.osman@outlook.com>
2024-02-24 23:35:15 -0500

    finish exercise

--- 07_interfaces/02-restaurants.ts
+++ 07_interfaces/02-restaurants.ts
@@ -7,4 +7,15 @@ interface RestaurantMap {
   [city: string]: string[];
 }
 
-const groupRestaurants = (restaurants: Restaurant[]): RestaurantMap => {};
+const groupRestaurants = (restaurants: Restaurant[]) => {
+  let restaurantMap: RestaurantMap = {};
+  for (let restaurant of restaurants) {
+    if (restaurantMap[restaurant.city]) {
+      restaurantMap[restaurant.city].push(restaurant.name);
+    } else {
+      restaurantMap[restaurant.city] = [restaurant.name];
+    }
+  }
+
+  return restaurantMap;
+};
