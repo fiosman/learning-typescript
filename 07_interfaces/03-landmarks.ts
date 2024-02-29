e60a35eb0877df9a27aac550a4c6d416ceb305bf
fiosman <fares.osman@outlook.com>
2024-02-28 22:09:42 -0500

    adds interfaces

--- 07_interfaces/03-landmarks.ts
+++ 07_interfaces/03-landmarks.ts
@@ -1,3 +1,43 @@
-type Landmark = {
-  Mountain | Park | Lighthouse | Lake | Waterfall | River | Fort
-};
+interface Mountain {
+  name: string;
+  height: number;
+  type: "mountain";
+}
+
+interface Park {
+  name: string;
+  acres: number;
+  type: "park";
+}
+
+interface Lighthouse {
+  name: string;
+  lit: number;
+  height: number;
+  type: "lighthouse";
+}
+
+interface Lake {
+  name: string;
+  miles: number;
+  type: "lake";
+}
+
+interface Fort {
+  type: "fort";
+}
+
+interface Waterfall {
+  name: string;
+  height: number;
+  type: "waterfall";
+}
+
+interface River {
+  name: string;
+  length: number;
+  depth: number;
+  type: "river";
+}
+
+type Landmark = Mountain | Park | Lighthouse | Lake | Waterfall | River | Fort;
