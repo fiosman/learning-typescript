85a7404f8fe227e9e1626c1875e601850d9ad4a0
fiosman <fares.osman@outlook.com>
2024-03-31 23:42:29 -0400

    finish switch

--- 07_interfaces/03-landmarks.ts
+++ 07_interfaces/03-landmarks.ts
@@ -46,15 +46,29 @@ type Landmark = Mountain | Park | Lighthouse | Lake | Waterfall | River | Fort;
 const describeLandmark = (landmark: Landmark): string => {
   let description: string = "";
 
-  switch (landmark?.type) {
+  switch (landmark.type) {
     case "fort":
       description = "";
+      break;
     case "waterfall":
+      description = `It is ${landmark.height} feet high.`;
+      break;
     case "river":
+      description = `It flows for ${landmark.length} miles and is ${landmark.depth} feet deep at its deepest.`;
+      break;
     case "lake":
+      description = `It covers ${landmark.miles} square miles of water.`;
+      break;
     case "lighthouse":
+      description = `It was first lit in ${landmark.lit} and is ${landmark.height} feet high.`;
+      break;
     case "park":
+      description = `It covers ${landmark.acres} square acres.`;
+      break;
     case "mountain":
+      description = `Its peak is ${landmark.height} feet high.`;
+      break;
   }
+
   return `${landmark.name} is a ${landmark.type} in Upstate New York.`;
 };
