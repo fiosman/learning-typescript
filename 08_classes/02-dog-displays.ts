10f421eb28c2d4c67645fc8939918251bcd88452
fiosman <fares.osman@outlook.com>
2024-04-12 23:34:36 -0400

    adds magic

--- 08_classes/02-dog-displays.ts
+++ 08_classes/02-dog-displays.ts
@@ -3,3 +3,14 @@ export interface PuppyInTheWindow {
   furriness: number;
   owner: string | undefined;
 }
+
+export class Puppy implements PuppyInTheWindow {
+  colors: string[];
+  furriness: number;
+  owner: string | undefined;
+
+  constructor(info: PuppyInTheWindow) {
+    this.colors = info.colors;
+    this.furriness = info.furriness;
+  }
+}
