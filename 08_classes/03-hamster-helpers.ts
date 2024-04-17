f33351cbe270bcd494589551c07c8d2287966851
fiosman <fares.osman@outlook.com>
2024-04-16 23:10:13 -0400

    adds class

--- 08_classes/03-hamster-helpers.ts
+++ 08_classes/03-hamster-helpers.ts
@@ -1,1 +1,11 @@
 export type SmallPetFood = "bugs" | "fruits" | "insects" | "plants" | "seeds" | "vegetables";
+
+export class SmallFurryPet {
+  readonly species: string;
+  protected happiness: number = 0;
+
+  constructor(species: string, happiness: number) {
+    this.species = species;
+    this.happiness = happiness;
+  }
+}
