9327134b10a1037685a516e6ab370192c40cf612
fiosman <fares.osman@outlook.com>
2024-05-06 23:02:49 -0400

    finish exercise

--- 09_type_modifiers/01-fabulous-secret.ts
+++ 09_type_modifiers/01-fabulous-secret.ts
@@ -6,4 +6,10 @@ interface Character {
 
 export const announceCharacter = (char: string) => {
   const formattedString = JSON.parse(char) as Character;
+
+  console.log(`I am ${formattedString.name}`);
+  console.log("My powers are: ", formattedString.powers.join(", "));
+  console.log(`I am ${formattedString.side}`);
+
+  return formattedString;
 };
